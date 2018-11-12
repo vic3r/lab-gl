5f1b8329c5d63c157ccc79c010a47524f0e333c1
vic3r <barcelonamane@hotmail.com>
2018-11-12 13:54:04 -0600

    Add stable version

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,5 +1,10 @@
 const planet = require('./planet');
 
 module.exports = (app) => {
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   app.use('/planet', planet);
 }
--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const config = require('./config');
 const bodyParser = require('body-parser');
 const routes = require('./app/routes/index');
 const express = require('express');
+const cors = require('cors');
 
 const app = new express();
-routes(app);
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+routes(app);
 
 const server = app.listen(config.port);
 const host = server.address().address;
